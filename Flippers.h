a2cc07a3b438e8a2bed6159db60f4b5b61c358a4
JanNetoLlorens <99729455+JanNetoLlorens@users.noreply.github.com>
2022-11-12 23:54:30 +0100

    flippers done

--- Flippers.h
+++ Flippers.h
@@ -7,7 +7,7 @@
 #include "Box2D/Box2D/Box2D.h"
 #include "SDL/include/SDL.h"
 
-class Flippers : public Entity	
+class Flippers : public Entity
 {
 public:
 	Flippers(Application* App);
@@ -28,5 +28,5 @@ public:
 	b2RevoluteJoint* joint2 = nullptr;
 private:
 	SDL_Texture* texture = nullptr;
-	const char* texturePath;	
+	const char* texturePath;
 };
